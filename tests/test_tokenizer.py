cf4831584a6f4e3213d17062a2821ad3bb3403d8
LaraRos <rostami.lara@gmail.com>
2025-05-30 12:11:33 +0200

    Linting

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,6 +1,7 @@
 import unittest
 from llm_hack.tokenizer import Tokenizer
 
+
 class TestTokenizer(unittest.TestCase):
 
     def test_encode(self):
@@ -11,21 +12,29 @@ class TestTokenizer(unittest.TestCase):
         self.assertEqual(tokenizer.encode("e b a"), [1, 5, 0])
 
     def test_decode(self):
-        tokenizer = Tokenizer("hello there! how are you? these are the tokens for this test")
+        tokenizer = Tokenizer(
+            "hello there! how are you? these are the tokens for this test"
+        )
         decoded = tokenizer.decode([6, 7, 3, 2])
         self.assertEqual(decoded, "test the for are")
-    
+
     def test_tokenize(self):
         tokenizer = Tokenizer()
-        self.assertEqual(tokenizer.tokenize("Hello, world!"), ["Hello", ",", "world", "!"])
+        self.assertEqual(
+            tokenizer.tokenize("Hello, world!"), ["Hello", ",", "world", "!"]
+        )
 
     def test_encode_decode(self):
-        tokenizer = Tokenizer("hello there! how are you? these are the tokens for this test")
+        tokenizer = Tokenizer(
+            "hello there! how are you? these are the tokens for this test"
+        )
         text = "hello tokens this test"
         self.assertEqual(text, tokenizer.decode(tokenizer.encode(text)))
 
     def test_encode_decode_unknown(self):
-        tokenizer = Tokenizer("hello there! how are you? these are the tokens for this test")
+        tokenizer = Tokenizer(
+            "hello there! how are you? these are the tokens for this test"
+        )
         text = "hello tokens this test unknown"
         result = "hello tokens this test <|unknown|>"
         self.assertEqual(result, tokenizer.decode(tokenizer.encode(text)))
@@ -34,6 +43,7 @@ class TestTokenizer(unittest.TestCase):
         tokenizer = Tokenizer("hello there!", "how are you?")
         text = "hello there! how are you?"
         self.assertEqual(text, tokenizer.decode(tokenizer.encode(text)))
+
 
 if __name__ == "__main__":
     unittest.main()
