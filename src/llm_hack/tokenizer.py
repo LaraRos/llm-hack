effac0a87a8a2b3821f65e5045d46673946e9549
LaraRos <rostami.lara@gmail.com>
2025-05-30 12:11:33 +0200

    Linting

--- src/llm_hack/tokenizer.py
+++ src/llm_hack/tokenizer.py
@@ -1,26 +1,31 @@
 import re
 from typing import List
 
+
 class Tokenizer:
     def __init__(self, *text: str):
 
         self.UNKNOWN = "<|unknown|>"
         self.ENDOFTEXT = "<|endoftext|>"
 
-        text = f' {self.ENDOFTEXT} '.join(text)
+        text = f" {self.ENDOFTEXT} ".join(text)
         tokenized_text = self.tokenize(text)
 
         tokens = sorted(set(tokenized_text))
         tokens.extend([self.UNKNOWN, self.ENDOFTEXT])
 
         vocab = list(enumerate(tokens))
-        
+
         self.str_to_int = {str(item): index for index, item in vocab}
         self.int_to_str = {index: str(item) for index, item in vocab}
 
     def get_encoded_token(self, token: str) -> str:
-        return self.str_to_int[token] if token in self.str_to_int else self.str_to_int[self.UNKNOWN]
-    
+        return (
+            self.str_to_int[token]
+            if token in self.str_to_int
+            else self.str_to_int[self.UNKNOWN]
+        )
+
     def get_decoded_token(self, encoded_token: int) -> int:
         return self.int_to_str[encoded_token]
 
@@ -29,11 +34,13 @@ class Tokenizer:
         return [self.get_encoded_token(token) for token in tokenized_text]
 
     def decode(self, encoded_text: List[int]) -> str:
-        text = " ".join([self.get_decoded_token(encoded_token) for encoded_token in encoded_text])
-        text = re.sub(r'\s+([,.:;?!()"_\'])', r'\1', text)
+        text = " ".join(
+            [self.get_decoded_token(encoded_token) for encoded_token in encoded_text]
+        )
+        text = re.sub(r'\s+([,.:;?!()"_\'])', r"\1", text)
         return text
 
     def tokenize(self, text: str) -> List[str]:
         split_text = re.split(r'(--|[,.:;?!()"_\']|\s)', text)
-        split_text = [item.strip() for item in split_text if item.strip()] 
+        split_text = [item.strip() for item in split_text if item.strip()]
         return split_text
